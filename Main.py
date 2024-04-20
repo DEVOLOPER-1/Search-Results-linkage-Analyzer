1f9b9e23e99876c5c786d107e518f0d2a9838d9f
Youssef Mohammad <s-youssef.hameid@zewailcity.edu.eg>
2024-04-20 12:43:46 +0200

    Ready for adding export to json

--- Main.py
+++ Main.py
@@ -1,15 +1,15 @@
-#pip install google-search-results
+# pip install google-search-results
 from serpapi import GoogleSearch
 import json
 
 User_Query = input("Enter your query: ")
-while(User_Query == "" or User_Query is None):
+while User_Query == "" or User_Query is None:
     User_Query = input("Invalid !!  Enter your query: ")
 
 params = {
     "engine": "google",
-    "q": User_Query ,
-    "api_key": "714b25eb147b43b3885fabb755c6a3682a48533965aaa15ed6b2a8492aff3a8e" ,
+    "q": User_Query,
+    "api_key": "714b25eb147b43b3885fabb755c6a3682a48533965aaa15ed6b2a8492aff3a8e",
 }
 
 
@@ -20,18 +20,28 @@ organic_results = results["organic_results"]
 organic_specific_results = {}
 positions = []
 titles = []
-#print(organic_results)
-for num , single_result in enumerate(organic_results):
-    print(f"Result {num+1}: {single_result["title"]} , Position {single_result["position"]} ")
+
+
+# Primitive Display for Results
+for num, single_result in enumerate(organic_results):
+    print(
+        f"Result {num+1}: {single_result["title"]} , Position {single_result["position"]} "
+    )
     print("____________________________________________________________")
 
+# Appending specified results to an empty lists
 for single_result in organic_results:
     titles.append(single_result["title"])
     positions.append(single_result["position"])
-    
 
+# Assigning the list of needed only results values  in the dictionary for json load
 organic_specific_results["Website_Name"] = titles
 organic_specific_results["Position_Priority"] = positions
 
-#parse_organic_results = json.loads()
 print(organic_specific_results)
+
+# Converting organic_specific_results dictionary into json
+json_object = json.dumps(
+    organic_specific_results, indent=4
+)  # I found that the indent value is to better read the output if printed
+print(json_object)
