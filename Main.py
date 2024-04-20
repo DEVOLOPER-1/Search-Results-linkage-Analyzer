50849927916ea936b24db7425ce17cf12a5d5ce0
Youssef Mohammad <s-youssef.hameid@zewailcity.edu.eg>
2024-04-20 10:45:26 +0200

    push

--- Main.py
+++ Main.py
@@ -2,4 +2,13 @@
 from serpapi import GoogleSearch
 
 User_Query = input("Enter your query: ")
+while(User_Query == ""):
+    User_Query = input("Invalid !!  Enter your query: ")
+
+params = {
+    "engine": "google",
+    "q": User_Query ,
+    "api_key": "714b25eb147b43b3885fabb755c6a3682a48533965aaa15ed6b2a8492aff3a8e"
+}
+
 
