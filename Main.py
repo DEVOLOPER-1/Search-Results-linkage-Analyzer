e1dec19087d4f6042e90a9ef3a0ebc8fdab629d0
Youssef Mohammad <s-youssef.hameid@zewailcity.edu.eg>
2024-04-20 10:59:25 +0200

    implemented basic serp api code

--- Main.py
+++ Main.py
@@ -11,4 +11,10 @@ params = {
     "api_key": "714b25eb147b43b3885fabb755c6a3682a48533965aaa15ed6b2a8492aff3a8e"
 }
 
-
+search = GoogleSearch(params)
+results = search.get_dict()
+organic_results = results["organic_results"]
+#print(organic_results)
+for num , single_result in enumerate(organic_results):
+    print(f"Result {num+1}: {single_result["title"]} , Position {single_result["position"]} ")
+    print("____________________________________________________________")
