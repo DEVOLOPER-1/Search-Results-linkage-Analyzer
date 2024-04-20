6f2488427f6818ae9e55316d2bacddf64e8dbf6c
Youssef Mohammad <s-youssef.hameid@zewailcity.edu.eg>
2024-04-20 15:50:38 +0200

    push edits

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ from serpapi import GoogleSearch
 import json
 
 User_Query = input("Enter your query: ")
-while User_Query == "" or User_Query is None:
+while User_Query == " " or User_Query is None:
     User_Query = input("Invalid !!  Enter your query: ")
 
 params = {
@@ -30,9 +30,10 @@ for num, single_result in enumerate(organic_results):
     print("____________________________________________________________")
 
 # Appending specified results to an empty lists
+# I made the  ###  as  a delimiter to extract the values of json file correctly
 for single_result in organic_results:
-    titles.append(single_result["title"])
-    positions.append(single_result["position"])
+    titles.append(f"{single_result["title"]}")
+    positions.append(f"{single_result["position"]}")
 
 # Assigning the list of needed only results values  in the dictionary for json load
 organic_specific_results["Website_Name"] = titles
@@ -41,7 +42,11 @@ organic_specific_results["Position_Priority"] = positions
 print(organic_specific_results)
 
 # Converting organic_specific_results dictionary into json
-json_object = json.dumps(
+json_object_organic_specific_results = json.dumps(
     organic_specific_results, indent=4
 )  # I found that the indent value is to better read the output if printed
-print(json_object)
+print(json_object_organic_specific_results)
+# write the JSON string to a file
+with open('organic_specific_results_with_no_ctr.json', 'w') as f:
+    f.write(json_object_organic_specific_results)
+    print("Done")
