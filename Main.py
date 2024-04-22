ce7f4cd4e68b9cb30e5a7d819657ca838e8346ac
Youssef Mohammad <s-youssef.hameid@zewailcity.edu.eg>
2024-04-22 16:25:04 +0200

    More logic edits with streamlit startup

--- Main.py
+++ Main.py
@@ -5,6 +5,10 @@ import statistics
 import networkx as nx
 import matplotlib.pyplot as plt
 import os  # I imported this library to check in future for a file that contain api key if it's not found the user will enter api key and saved in running dir and in next runtime the script will extract token from saved file in same directory
+import streamlit as st
+import pandas as pd
+import altair as alt
+import plotly.express as px
 
 
 # Create an instance of the class
@@ -139,12 +143,17 @@ for single_snippet_tuple in snippets_dict.values():
         Total_TF += round(TF, 1)  # Approximation of TF
     TF_Values_tuple.append(Total_TF)
 
-for index, element in enumerate(organic_results):
-    key = element["title"]
-    if key not in relevancy_valued_dict_2:
-        relevancy_valued_dict_2[key] = TF_Values_tuple[index]
-    if key in relevancy_valued_dict_2:
-        relevancy_valued_dict_2[key] += TF_Values_tuple[index]
+num_of_index = 0
+for key , value in (snippets_dict.items()): #we can put either .keys() or .items()
+    temp_key = key
+    if temp_key not in relevancy_valued_dict_2:
+            if num_of_index < len(TF_Values_tuple):
+                relevancy_valued_dict_2[temp_key] = TF_Values_tuple[num_of_index]
+                num_of_index += 1 
+    if temp_key in relevancy_valued_dict_2:
+        if num_of_index < len(TF_Values_tuple):
+            relevancy_valued_dict_2[temp_key] += TF_Values_tuple[num_of_index]
+            num_of_index += 1               
 
 print(relevancy_valued_dict_2)
 
@@ -201,3 +210,20 @@ nx.draw(
 plt.axis("off")
 # plt.show()
 plt.savefig("Network Graph.jpeg", format="JPEG", bbox_inches="tight", dpi=1800)
+
+
+
+
+
+
+
+
+
+
+
+
+############################Stream_lit Dashboard Part########################
+#Page configuration
+file = open("website_title_with_position.json")
+dt = json.load(file)
+st.json(dt)
