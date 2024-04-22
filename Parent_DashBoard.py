a2be0eec3a39f88384a1dd0e2d9bdf4f06711390
Youssef Mohammad <s-youssef.hameid@zewailcity.edu.eg>
2024-04-23 00:45:34 +0200

    Made Columns in DashBoard

--- Parent_DashBoard.py
+++ Parent_DashBoard.py
@@ -16,23 +16,37 @@ st.set_page_config(
     },
 )
 
-st.header("Welcome to Our :green[Dashboard]  :wave:", divider="rainbow")
+st.header("Welcome to :green[Search-Results-linkage-Analyzer]  :wave:", divider="rainbow")
 
 
 # file Import
 
 with open("website_title_with_position.json") as f:
-    data = json.load(f)  # note the difference between loads and load here
+    data = json.load(f)  
+
+with open("website_title_with_relevancy_values_2.json") as f:  
+    data_2 = json.load(f)
 
 website_title_with_position_df = pd.DataFrame.from_dict(data, orient="index")
 # table = st.table(website_title_with_position_df) Just for testing purpose and it makes a print function
+website_title_with_relevancy_values_df =pd.DataFrame.from_dict(data_2, orient="index")
 
-st.title("According to your query there are the :green[results]")
-st.subheader("Lower values means Higher :green[priority]")
-st.bar_chart(
-    data=website_title_with_position_df, 
-    width=0, 
-    height=0, 
-    use_container_width=True,
-    color = "#4F6F52"
-)
+st.title("According to your query there are the :green[results] :point_down:")
+
+#Column Objects in the main Dash Board
+
+Priority , Relevancy , Network_Graph = st.columns(3)
+
+#The container of each column
+#Priority Bar Chart container
+with Priority:
+    with st.container(border=True):
+        st.title("Results :blue[Priority] Bar Chart")
+        st.subheader(":red[Lower] values :arrow_forward: Higher :green[priority]")
+        st.bar_chart(
+            data=website_title_with_position_df, 
+            width=0, 
+            height=0, 
+            use_container_width=True,
+            color = "#4F6F52"
+        )
